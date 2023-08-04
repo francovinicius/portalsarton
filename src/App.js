3204a2594c6c59482de004acb814f66f710344c4
francovinicius <duartevini.dev@gmail.com>
2023-08-04 09:55:02 -0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+
+import Cards from './Components/Cards';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      a
+      <Header />
+      <Cards/>
+      <Footer />
     </div>
   );
 }
