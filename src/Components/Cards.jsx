62b304fa9b60381e5e648a5204288cc76a246014
francovinicius <duartevini.dev@gmail.com>
2023-08-04 11:36:22 -0300

    add cards

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
 
 const Cards = () => {
   return (
-    <div className='d-flex justfy-content-center align-items-center'>
-        <div className="teste">ts</div>
+    <div className="cards d-flex justify-content-center align-items-center gap-5 flex-wrap">
+      <div className="card-model d-flex justify-content-center flex-wrap">
+
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+          <h6 className="h-model">
+            Planilha de cadastro
+          </h6>
+          <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+        </a>
+      </div>
+      <div className="card-model d-flex justify-content-center flex-wrap">
+
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+          <h6 className="h-model">
+            OneDrive
+          </h6>
+          <img className="card-img" src="./assets/icons/onedrive.svg" alt="excel" />
+        </a>
+      </div>
+      <div className="card-model d-flex justify-content-center flex-wrap">
+
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+          <h6 className="h-model h-model-l">
+            A.R.C.A
+          </h6>
+          <img className="card-img l-img" src="./assets/icons/logo-sarton.svg" alt="excel" />
+        </a>
+      </div>
+      <div className="card-model d-flex justify-content-center flex-wrap">
+
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+          <h6 className="h-model">
+            WebMail
+          </h6>
+          <img className="card-img" src="./assets/icons/webmail.png" alt="excel" />
+        </a>
+      </div>
+
+
+
+
     </div>
   )
 }
