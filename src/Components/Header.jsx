e615c44263df8b7303df9e5aff3222903c5ab19a
francovinicius <duartevini.dev@gmail.com>
2023-08-07 13:18:52 -0300

    Alt navbar span

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
                 <div className="container-fluid">
                     <a className="navbar-brand d-flex gap-3" href="/">
                         <img src="/assets/icons/logo-sarton.svg" alt="" className="img-nav d-inline-block align-text-top" />
-                        <span>Portal Sarton</span>
+                        <span className="tittle-nav">PSI -Portal de Sistemas Integrados</span>
                     </a>
                 </div>
             </nav>
