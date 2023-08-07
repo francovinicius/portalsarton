9847fdd3ae2f9782b32da209b094aba95011fd58
francovinicius <duartevini.dev@gmail.com>
2023-08-07 13:58:37 -0300

    Add links

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,24 +2,47 @@
 const Cards = () => {
   return (
     <div className="cards d-flex justify-content-center align-items-center gap-5 flex-wrap">
+      {/* Cadastro */}
       <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1GXY5tMQqONWklF2-e1_Gndisquvts0xk1fUfCtck7Bk/edit?pli=1#gid=1089243014" target="_blank">
           <h6 className="h-model">
             Planilha de cadastro
           </h6>
           <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
         </a>
       </div>
+      {/* ASO */}
       <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1_oSbxUFArBQQN57cH2qT3P7xRCM_sLBxDwLj8JdUbpc/edit#gid=668875208" target="_blank">
+          <h6 className="h-model">
+            Planilha de ASOs
+          </h6>
+          <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+        </a>
+      </div>
+      {/* Adv Form */}
+      <div className="card-model d-flex justify-content-center flex-wrap">
+
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLScx4KHyEepdSaJufXqqCEW-c1eRyNd3X_wBJR_Jq8gFjGpPsw/viewform" target="_blank">
+          <h6 className="h-model">
+            Advertência Form
+          </h6>
+          <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
+        </a>
+      </div>
+      {/* Pat */}
+      <div className="card-model d-flex justify-content-center flex-wrap">
+
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1qPPUkVuGLBOrmJjGOWXbS4FMvwDeEjf1t7paY7jZzQM/edit#gid=988867197" target="_blank">
           <h6 className="h-model">
             Patrimônio
           </h6>
           <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
         </a>
       </div>
+      {/* Pat Form */}
       <div className="card-model d-flex justify-content-center flex-wrap">
 
         <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
@@ -29,27 +52,30 @@ const Cards = () => {
           <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
         </a>
       </div>
+      {/* OneDrive */}
       <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://onedrive.live.com/?id=root&cid=D60D375BD4296DB8&qt=sharedby" target="_blank">
           <h6 className="h-model">
             OneDrive
           </h6>
           <img className="card-img" src="./assets/icons/onedrive.svg" alt="excel" />
         </a>
       </div>
+      {/* ARCA */}
       <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://sarton.com.br/arca/login/" target="_blank">
           <h6 className="h-model h-model-l">
             A.R.C.A
           </h6>
           <img className="card-img l-img" src="./assets/icons/logo-sarton.svg" alt="excel" />
         </a>
       </div>
+      {/* Email */}
       <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="/" target="_blank">
+        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://sarton.com.br:2096/" target="_blank">
           <h6 className="h-model">
             WebMail
           </h6>
