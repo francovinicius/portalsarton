b27e0548fb7e7cf42ed93667b0fb66c42a00fad5
francovinicius <duartevini.dev@gmail.com>
2023-12-13 11:03:46 -0300

    add novos links

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,152 +1,227 @@
-
+import '../index.css';
 const Cards = () => {
   return (
-    <div className="cards d-flex justify-content-center align-items-center gap-5 flex-wrap">
-      {/* Cadastro 
-      <div className="card-model d-flex justify-content-center flex-wrap">
+    <div className="cards d-flex flex-column justify-content-center align-items-center gap-5 flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1GXY5tMQqONWklF2-e1_Gndisquvts0xk1fUfCtck7Bk/edit?pli=1#gid=1089243014" target="_blank">
-          <h6 className="h-model">
-            Planilha de cadastro
-          </h6>
-          <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
-        </a>
-      </div>
-      */}
-      {/* Cruzamento Ago */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+      {/* Ut Diaria */}
+      <div className="cards d-flex flex-column justify-content-center align-items-center gap-5 flex-wrap">
+        <h1 className="title-psi">Utilizações diárias</h1>
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/14bm3Yxf1bz5ZmK_dvbvXtPojHwHRFUNLTQDTYQp25hk/edit#gid=953444778" target="_blank">
-          <h6 className="h-model">
-          Cruzamento Ago
-          </h6>
-          <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
-        </a>
-      </div>
-      
-      {/* Banco de Talentos */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://onedrive.live.com/edit.aspx?resid=D60D375BD4296DB8!85233&cid=d60d375bd4296db8&CT=1691507723125&OR=ItemsView" target="_blank">
-          <h6 className="h-model">
-            Talentos
-          </h6>
-          <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
-        </a>
-      </div>
-      {/* ASO */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1_oSbxUFArBQQN57cH2qT3P7xRCM_sLBxDwLj8JdUbpc/edit#gid=668875208" target="_blank">
-          <h6 className="h-model">
-            Planilha de ASOs
-          </h6>
-          <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
-        </a>
-      </div>
-      {/* Adv Form */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+        <div className='className="cards d-flex justify-content-center align-items-center gap-5 flex-wrap"'>
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLScx4KHyEepdSaJufXqqCEW-c1eRyNd3X_wBJR_Jq8gFjGpPsw/viewform" target="_blank">
-          <h6 className="h-model">
-            Advertência Form
-          </h6>
-          <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
-        </a>
-      </div>
-      {/* Pat */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+          {/* Cadastro  */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1qPPUkVuGLBOrmJjGOWXbS4FMvwDeEjf1t7paY7jZzQM/edit#gid=988867197" target="_blank">
-          <h6 className="h-model">
-            Patrimônio
-          </h6>
-          <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
-        </a>
-      </div>
-      {/* Pat Form */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1GXY5tMQqONWklF2-e1_Gndisquvts0xk1fUfCtck7Bk/edit?pli=1#gid=1089243014" target="_blank">
+              <h6 className="h-model">
+                Planilha de cadastro
+              </h6>
+              <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+            </a>
+          </div>
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLSefDN9ruSxXQgYT3lP1XcUZ1MNH42v135XaGgGiKqxD6PPCxw/viewform" target="_blank">
-          <h6 className="h-model">
-            Patrimônio Form
-          </h6>
-          <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
-        </a>
-      </div>
-      {/* Manutenção Veicular Form */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+          {/* Acuttis */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdlEln08jvO0zSRZVS4MgN6p8tuOSTHxOkAYPN60kTKQL80tg/viewform" target="_blank">
-          <h6 className="h-model">
-            Car Maintenance
-          </h6>
-          <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
-        </a>
-      </div>
-      {/* Veiculo Form */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://app.acuttis.com.br/signin?_ga=2.82562390.134971841.1691433937-37901974.1691433937&_gl=1*zmsbzd*_ga*Mzc5MDE5NzQuMTY5MTQzMzkzNw..*_ga_1KK40SNS75*MTY5MTQzMzkzNi4xLjAuMTY5MTQzMzk0NC4wLjAuMA..*_ga_43W2WYML5H*MTY5MTQzMzkzOC4xLjAuMTY5MTQzMzk0Ni4wLjAuMA.." target="_blank">
+              <h6 className="h-model">
+                Acuttis
+              </h6>
+              <img className="card-img" src="./assets/icons/acuttis.svg" alt="excel" />
+            </a>
+          </div>
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLScmKccid3JuqpobYzf0HgkFLLPesCplDPL9GiKru4cprjYunQ/viewform" target="_blank">
-          <h6 className="h-model">
-            Veículo
-          </h6>
-          <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
-        </a>
-      </div>
-      {/* Abastecimento Form */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdWYlSVl095QUezUHdEYV_eXPsgyhqWt6F5jtdOJmTpyS2zEw/viewform" target="_blank">
-          <h6 className="h-model">
-            Abastecimento
-          </h6>
-          <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
-        </a>
-      </div>
-      {/* OneDrive */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+          {/* Cruzamento  */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://onedrive.live.com/?id=root&cid=D60D375BD4296DB8&qt=sharedby" target="_blank">
-          <h6 className="h-model">
-            OneDrive
-          </h6>
-          <img className="card-img" src="./assets/icons/onedrive.svg" alt="excel" />
-        </a>
-      </div>
-      {/* ARCA */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+            <a className="card-a d-flex flex-column justify-content-center align-items-center"
+              href="https://docs.google.com/spreadsheets/d/1BgtUf5yx0x27Hecn8okY_0wh_PhzVjZ1n-oJ1On94qo/edit#gid=1103674570" target="_blank">
+              <h6 className="h-model">
+                Cruzamento
+              </h6>
+              <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+            </a>
+          </div>
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://sarton.com.br/arca/login/" target="_blank">
-          <h6 className="h-model h-model-l">
-            A.R.C.A
-          </h6>
-          <img className="card-img l-img" src="./assets/icons/logo-sarton.svg" alt="excel" />
-        </a>
-      </div>
-      {/* Email */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+          {/* Relatórios Diários */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+            <a className="card-a d-flex flex-column justify-content-center align-items-center"
+              href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAa__cJ81rhURFY3RjBRUDJYQ0UzODROTUVJSDFIOFdQQy4u" target="_blank">
+              <h6 className="h-model">
+                Relatório Serviços
+              </h6>
+              <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
+            </a>
+          </div>
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://sarton.com.br:2096/" target="_blank">
-          <h6 className="h-model">
-            WebMail
-          </h6>
-          <img className="card-img" src="./assets/icons/webmail.png" alt="excel" />
-        </a>
-      </div>
-      {/* Acuttis */}
-      <div className="card-model d-flex justify-content-center flex-wrap">
+          {/* Cadastro  */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
 
-        <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://app.acuttis.com.br/signin?_ga=2.82562390.134971841.1691433937-37901974.1691433937&_gl=1*zmsbzd*_ga*Mzc5MDE5NzQuMTY5MTQzMzkzNw..*_ga_1KK40SNS75*MTY5MTQzMzkzNi4xLjAuMTY5MTQzMzk0NC4wLjAuMA..*_ga_43W2WYML5H*MTY5MTQzMzkzOC4xLjAuMTY5MTQzMzk0Ni4wLjAuMA.." target="_blank">
-          <h6 className="h-model">
-            Acuttis
-          </h6>
-          <img className="card-img" src="./assets/icons/acuttis.svg" alt="excel" />
-        </a>
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1GXY5tMQqONWklF2-e1_Gndisquvts0xk1fUfCtck7Bk/edit?pli=1#gid=1089243014" target="_blank">
+              <h6 className="h-model">
+                Onvio
+              </h6>
+              <img className="card-img-2" src="./assets/icons/rede.png" alt="excel" />
+            </a>
+          </div>  
+
+          {/* ARCA */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://sarton.com.br/arca/login/" target="_blank">
+              <h6 className="h-model h-model-l">
+                A.R.C.A
+              </h6>
+              <img className="card-img l-img" src="./assets/icons/logo-sarton.svg" alt="excel" />
+            </a>
+          </div>
+          {/* Email */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://sarton.com.br:2096/" target="_blank">
+              <h6 className="h-model">
+                WebMail
+              </h6>
+              <img className="card-img" src="./assets/icons/webmail.png" alt="excel" />
+            </a>
+          </div>
+
+
+        </div>
+
       </div>
 
+      {/* Acompanhamentos */}
+      <div className="cards d-flex flex-column justify-content-center align-items-center gap-5 flex-wrap">
+        <h1 className="title-psi">Acompanhamentos</h1>
 
 
+        <div className='className="cards d-flex justify-content-center align-items-center gap-5 flex-wrap"'>
+
+          {/* Cartões Alelo */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center"
+              href="https://docs.google.com/spreadsheets/d/1qOqEuCOSfF-JGyDUxZg68P30i6WgmW0F9fTtWrwPIYw/edit#gid=232315231" target="_blank">
+              <h6 className="h-model">
+                Alelo Cartões
+              </h6>
+              <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+            </a>
+          </div>
+
+          {/* Banco de Talentos */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://onedrive.live.com/edit.aspx?resid=D60D375BD4296DB8!85233&cid=d60d375bd4296db8&CT=1691507723125&OR=ItemsView" target="_blank">
+              <h6 className="h-model">
+                Talentos
+              </h6>
+              <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+            </a>
+          </div>
+          {/* ASO */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1_oSbxUFArBQQN57cH2qT3P7xRCM_sLBxDwLj8JdUbpc/edit#gid=668875208" target="_blank">
+              <h6 className="h-model">
+                Planilha de ASOs
+              </h6>
+              <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+            </a>
+          </div>
+
+          {/* Pat */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/spreadsheets/d/1qPPUkVuGLBOrmJjGOWXbS4FMvwDeEjf1t7paY7jZzQM/edit#gid=988867197" target="_blank">
+              <h6 className="h-model">
+                Patrimônio
+              </h6>
+              <img className="card-img" src="./assets/icons/excel.svg" alt="excel" />
+            </a>
+          </div>
+
+          {/* OneDrive */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://onedrive.live.com/?id=root&cid=D60D375BD4296DB8&qt=sharedby" target="_blank">
+              <h6 className="h-model">
+                OneDrive
+              </h6>
+              <img className="card-img" src="./assets/icons/onedrive.svg" alt="excel" />
+            </a>
+          </div>
+
+
+        </div>
+
+
+
+      </div>
+
+      {/* Forms */}
+      <div className="cards d-flex flex-column justify-content-center align-items-center gap-5 flex-wrap">
+        <h1 className="title-psi">Formulários para envio</h1>
+
+        <div className='className="cards d-flex justify-content-center align-items-center gap-5 flex-wrap"'>
+
+          {/* Adv Form */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLScx4KHyEepdSaJufXqqCEW-c1eRyNd3X_wBJR_Jq8gFjGpPsw/viewform" target="_blank">
+              <h6 className="h-model">
+                Advertência Form
+              </h6>
+              <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
+            </a>
+          </div>
+
+          {/* Pat Form */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLSefDN9ruSxXQgYT3lP1XcUZ1MNH42v135XaGgGiKqxD6PPCxw/viewform" target="_blank">
+              <h6 className="h-model">
+                Patrimônio Form
+              </h6>
+              <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
+            </a>
+          </div>
+          {/* Manutenção Veicular Form */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdlEln08jvO0zSRZVS4MgN6p8tuOSTHxOkAYPN60kTKQL80tg/viewform" target="_blank">
+              <h6 className="h-model">
+                Car Maintenance
+              </h6>
+              <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
+            </a>
+          </div>
+          {/* Veiculo Form */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLScmKccid3JuqpobYzf0HgkFLLPesCplDPL9GiKru4cprjYunQ/viewform" target="_blank">
+              <h6 className="h-model">
+                Veículo
+              </h6>
+              <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
+            </a>
+          </div>
+          {/* Abastecimento Form */}
+          <div className="card-model d-flex justify-content-center flex-wrap">
+
+            <a className="card-a d-flex flex-column justify-content-center align-items-center" href="https://docs.google.com/forms/d/e/1FAIpQLSdWYlSVl095QUezUHdEYV_eXPsgyhqWt6F5jtdOJmTpyS2zEw/viewform" target="_blank">
+              <h6 className="h-model">
+                Abastecimento
+              </h6>
+              <img className="card-img" src="./assets/icons/form.svg" alt="excel" />
+            </a>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
